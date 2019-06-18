b51bbdf68a09c1ca783df146502a1cdbfc6bedac
黄永好 <yonghao.huang@mtime.com>
2019-06-18 17:14:08 +0800

    对接淘宝商品列表搜索接口

--- com-platform-front-service/src/test/java/com/platform/front/service/FrontServiceApplicationTests.java
+++ com-platform-front-service/src/test/java/com/platform/front/service/FrontServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.platform.front.service;
 
+import com.platform.front.service.iface.TaobaoGoodsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FrontServiceApplication.class)
 public class FrontServiceApplicationTests {
+
+    @Autowired
+    TaobaoGoodsService taobaoGoodsService;
 
     @Test
     public void contextLoads() {
