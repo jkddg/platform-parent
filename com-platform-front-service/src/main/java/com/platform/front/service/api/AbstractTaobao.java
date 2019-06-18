772ebd988ad106afc9da0060eb82506c1ef4a788
黄永好 <yonghao.huang@mtime.com>
2019-06-18 19:01:10 +0800

    配置读取

--- com-platform-front-service/src/main/java/com/platform/front/service/api/AbstractTaobao.java
+++ com-platform-front-service/src/main/java/com/platform/front/service/api/AbstractTaobao.java
@@ -1,15 +1,24 @@
 package com.platform.front.service.api;
 
+import com.platform.front.service.config.TaobaoConfig;
+import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Huangyonghao on 2019/6/18 14:50.
  */
+@Component
 public class AbstractTaobao {
+
+    @Autowired
+    protected TaobaoConfig taobaoConfig;
+
     protected TaobaoClient getTaobaoClient(){
-//        TaobaoConfig config = appPropertiesConfig.getTaobaoConfig();
-//        TaobaoClient client = new DefaultTaobaoClient(config.getTbkUrl(), config.getTbkAppkey(), config.getTbkSecret());
-//        return client;
-        return null;
+
+        TaobaoClient client = new DefaultTaobaoClient(taobaoConfig.getTbkUrl(), taobaoConfig.getTbkAppkey(), taobaoConfig.getTbkSecret());
+        return client;
+
     }
 }
