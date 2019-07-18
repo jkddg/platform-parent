19407947263cd777b25e63be297d9e93a401bb86
黄永好 <yonghao.huang@mtime.com>
2019-07-18 10:34:01 +0800

    修改appkey

--- com-platform-admin-service/src/test/java/com/platform/admin/service/FrontServiceApplicationTests.java
+++ com-platform-admin-service/src/test/java/com/platform/admin/service/FrontServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.platform.admin.service;
 
-import com.platform.front.service.iface.GoodsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AdminServiceApplication.class)
 public class FrontServiceApplicationTests {
 
-    @Autowired
-    GoodsService taobaoGoodsService;
+
 
     @Test
     public void contextLoads() {
