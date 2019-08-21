07517693c46cf22179b49dbe392f06497d4198d4
黄永好 <yonghao.huang@mtime.com>
2019-08-21 17:46:19 +0800

    淘宝商品同步task

--- com-platform-admin-service/src/main/java/com/platform/admin/service/mapper/TbGoodsMapper.java
+++ com-platform-admin-service/src/main/java/com/platform/admin/service/mapper/TbGoodsMapper.java
@@ -27,23 +27,26 @@ public class TbGoodsMapper {
             goodsInfo.setPlatformName(PlatformEnum.TAOBAO.displayName());
         }
         goodsInfo.setUpdateTime(LocalDateTime.now());
-        if(!StringUtils.isEmpty(data.getReservePrice())) {
+        if (!StringUtils.isEmpty(data.getReservePrice())) {
             goodsInfo.setReservePrice(Double.parseDouble(data.getReservePrice()));
         }
-        if(!StringUtils.isEmpty(data.getZkFinalPrice())) {
+        if (!StringUtils.isEmpty(data.getZkFinalPrice())) {
             goodsInfo.setZkFinalPrice(Double.parseDouble(data.getZkFinalPrice()));
         }
-        if(!StringUtils.isEmpty(data.getCouponAmount())) {
+        if (!StringUtils.isEmpty(data.getCouponAmount())) {
             goodsInfo.setCouponAmount(Double.parseDouble(data.getCouponAmount()));
         }
-        if(!StringUtils.isEmpty(data.getCouponStartFee())) {
+        if (!StringUtils.isEmpty(data.getCouponStartFee())) {
             goodsInfo.setCouponStartFee(Double.parseDouble(data.getCouponStartFee()));
         }
-        if(!StringUtils.isEmpty(data.getCouponStartTime())) {
+        if (!StringUtils.isEmpty(data.getCouponStartTime())) {
             goodsInfo.setCouponStartTime(DateUtil.dateStr2StartLocalDateTime(data.getCouponStartTime()));
         }
-        if(!StringUtils.isEmpty(data.getCouponEndTime())) {
+        if (!StringUtils.isEmpty(data.getCouponEndTime())) {
             goodsInfo.setCouponEndTime(DateUtil.dateStr2EndLocalDateTime(data.getCouponEndTime()));
+        }
+        if ("0.00".equals(goodsInfo.getFinalPrice())) {
+            return null;
         }
         return goodsInfo;
     }
