5aaefec740ccb859a767fd36ff1564160243b3f9
黄永好 <yonghao.huang@mtime.com>
2020-05-19 17:22:39 +0800

    测试断路器

--- com-platform-admin-service/src/main/java/com/platform/admin/service/controller/ManualMessageController.java
+++ com-platform-admin-service/src/main/java/com/platform/admin/service/controller/ManualMessageController.java
@@ -1,6 +1,9 @@
 package com.platform.admin.service.controller;
 
+import com.platform.admin.service.iface.ManualMessageService;
 import com.platform.common.modal.ResultInfo;
+import com.platform.common.modal.manual.MessageParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/manual/message")
 public class ManualMessageController {
 
-    @RequestMapping("add")
-    public ResultInfo addMessage(){
-        ResultInfo resultInfo=new ResultInfo();
+    @Autowired
+    ManualMessageService manualMessageService;
 
+    @RequestMapping("add")
+    public ResultInfo addMessage(MessageParam messageParam) {
+        ResultInfo resultInfo = manualMessageService.add(messageParam);
         return resultInfo;
     }
 }
