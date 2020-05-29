7a1f1669558326b2ae482243410aca9fb4ac0a1e
黄永好 <yonghao.huang@mtime.com>
2020-05-29 16:12:43 +0800

    zipkinServer

--- com-platform-admin-service/src/main/java/com/platform/admin/service/AdminServiceApplication.java
+++ com-platform-admin-service/src/main/java/com/platform/admin/service/AdminServiceApplication.java
@@ -3,13 +3,14 @@ package com.platform.admin.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 
 /**
  * @author huangyonghao
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServiceApplication {
 
--- com-platform-zipkin-server/src/main/java/com/platform/zipkin/server/ZipkinApplication.java
+++ com-platform-zipkin-server/src/main/java/com/platform/zipkin/server/ZipkinApplication.java
@@ -2,9 +2,17 @@ package com.platform.zipkin.server;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
-@EnableZipkinServer
+
+/**
+ * @author jkddg
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class, args);
